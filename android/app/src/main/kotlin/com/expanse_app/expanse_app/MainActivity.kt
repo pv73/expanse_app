8fba8f2e025f82766de5d0b67c652e8abde95d72
Prashant Verma <mailtoprashant73@gmail.com>
2023-05-27 16:19:49 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/expanse_app/expanse_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.expanse_app.expanse_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home_Page extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Container(),
+    );
+  }
+
+}
